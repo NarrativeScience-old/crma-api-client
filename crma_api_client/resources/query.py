36f837ad73ea2964a7b4386b7716e3a5e1326339
Jonathan Drake <jdrake@narrativescience.com>
2022-04-12 16:10:59 -0700

    LEXIO-37876 Adds richer metadata model to query resource (#2)
    * LEXIO-37876 Adds richer metadata model to query resource
    
    * Version bumped to 0.3.0
    
    Co-authored-by: ns-circle-ci <devops-team+circleci@narrativescience.com>

--- crma_api_client/resources/query.py
+++ crma_api_client/resources/query.py
@@ -16,20 +16,69 @@ class QueryLanguage(str, Enum):
     sql = "SQL"
 
 
+class ProjectionField(BaseModel):
+    """Field projected in the query result"""
+
+    id: str
+    type: str
+
+    @property
+    def name(self) -> str:
+        """Return field name that omits stream reference"""
+        return self.id.split(".")[-1]
+
+
+class LineageProjection(BaseModel):
+    """Field projection metadata container"""
+
+    field: ProjectionField
+
+
+class QueryLineage(BaseModel):
+    """Lineage that describes field projections in a query result"""
+
+    type: str
+    projections: List[LineageProjection]
+
+
+class QueryResultsMetadata(BaseModel):
+    """Query results metadata"""
+
+    lineage: QueryLineage
+    query_language: QueryLanguage
+
+    class Config:
+        """Model configuration"""
+
+        alias_generator = to_camel
+
+
 class QueryResults(BaseModel):
     """Query results model"""
 
+    metadata: List[QueryResultsMetadata]
     records: List[Dict[str, Any]]
 
 
 class QueryResponse(BaseModel):
-    """Response model for the query resource"""
+    """Response model for the query resource
+
+    See https://developer.salesforce.com/docs/atlas.en-us.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_resources_query.htm
+    """
 
     action: str
     response_id: str
     results: QueryResults
     query: str
     response_time: int
+
+    @property
+    def fields(self) -> List[ProjectionField]:
+        """Return the fields from the query response metadata
+
+        This assumes there is only one metadata object and one lineage object.
+        """
+        return [p.field for p in self.results.metadata[0].lineage.projections]
 
     class Config:
         """Model configuration"""

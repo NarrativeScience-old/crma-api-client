c4c0a677a990a24c5fef6773845891a67fd95a70
Stephen Hudson <shudson@narrativescience.com>
2022-04-25 08:43:45 -0500

    LEXIO-37872: get_dataset_version

--- crma_api_client/resources/dataset.py
+++ crma_api_client/resources/dataset.py
@@ -1,9 +1,10 @@
 """Contains definitions for the dataset resource"""
 
 from datetime import datetime
-from typing import List
+from enum import Enum
+from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from .user import User
 from .util import to_camel
@@ -14,6 +15,114 @@ class Dataset(BaseModel):
 
     id: str
     url: str
+
+
+class DateType(Enum):
+    """The type of date field"""
+
+    date = "Date"
+    date_only = "DateOnly"
+    date_time = "DateTime"
+
+
+class XmdDateFields(BaseModel):
+    """Date fields in the extended metadata format
+
+    See https://developer.salesforce.com/docs/atlas.en-us.230.0.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_resources_xmd_main.htm#XmdDateFieldRepresentation
+    """
+
+    day: str
+    epoch_day: str
+    epoch_second: str
+    fiscal_month: Optional[str]
+    fiscal_quarter: Optional[str]
+    fiscal_week: Optional[str]
+    fiscal_year: Optional[str]
+    full_field: str
+    hour: str
+    minute: str
+    month: str
+    quarter: str
+    second: str
+    week: str
+    year: str
+
+    @validator("fiscal_month", "fiscal_quarter", "fiscal_week", "fiscal_year")
+    def not_none(cls, v):
+        """The value should not be None"""
+        if v is None:
+            raise ValueError("Fiscal fields may not be None")
+        return v
+
+    class Config:
+        """Model configuration"""
+
+        alias_generator = to_camel
+
+
+class XmdDate(BaseModel):
+    """Date in the extended metadata format
+
+    See https://developer.salesforce.com/docs/atlas.en-us.230.0.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_resources_xmd_main.htm#XmdDateRepresentation
+    """
+
+    alias: str
+    fields: XmdDateFields
+    first_day_of_week: int
+    fiscal_month_offset: int
+    fully_qualified_name: str
+    is_year_end_fiscal_year: bool
+    label: str
+    type: DateType
+
+    class Config:
+        """Model configuration"""
+
+        alias_generator = to_camel
+
+
+class XmdDimension(BaseModel):
+    """Dimension in the Xmd format
+
+    See https://developer.salesforce.com/docs/atlas.en-us.230.0.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_resources_xmd_main.htm#XmdDimensionRepresentation
+    """
+
+    field: str
+    label: str
+
+
+class XmdMeasure(BaseModel):
+    """Measure in the Xmd format
+
+    See https://developer.salesforce.com/docs/atlas.en-us.230.0.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_resources_xmd_main.htm#XmdMeasureRepresentation
+    """
+
+    field: str
+    label: str
+
+
+class DatasetXmd(BaseModel):
+    """Extended metadata for a dataset
+
+    See https://developer.salesforce.com/docs/atlas.en-us.230.0.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_resources_xmd_main.htm
+    """
+
+    created_by: User
+    created_date: datetime
+    dates: List[XmdDate]
+    derived_dimensions: List[XmdDimension]
+    derived_measures: List[XmdMeasure]
+    dimensions: List[XmdDimension]
+    last_modified_by: User
+    last_modified_date: datetime
+    measures: List[XmdMeasure]
+    type: str
+    url: str
+
+    class Config:
+        """Model configuration"""
+
+        alias_generator = to_camel
 
 
 class DatasetVersion(BaseModel):
@@ -36,6 +145,15 @@ class DatasetVersion(BaseModel):
         """Model configuration"""
 
         alias_generator = to_camel
+
+
+class DatasetVersionResponse(DatasetVersion):
+    """Dataset version model
+
+    See https://developer.salesforce.com/docs/atlas.en-us.bi_dev_guide_rest.meta/bi_dev_guide_rest/bi_responses_dataset_version.htm
+    """
+
+    xmd_main: DatasetXmd
 
 
 class DatasetVersionsResponse(BaseModel):
---
+++ tests/functional/test_dataset.py
@@ -0,0 +1,14 @@
+"""Contains functional tests for the dataset resource"""
+
+import pytest
+
+from crma_api_client.client import CRMAAPIClient
+
+
+@pytest.mark.asyncio
+async def test_query(client: CRMAAPIClient):
+    """Should run a simple query"""
+    dataset_id = "Sample_Superstore_xls_Orders"
+    versions_response = await client.list_dataset_versions(dataset_id)
+    version = versions_response.versions[0]
+    await client.get_dataset_version(dataset_id, version.id)
